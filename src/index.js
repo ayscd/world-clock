0a65e88f8861a045c79dfbd313dfe2dca0ceeda4
ayscd <ayeskacsta@gmail.com>
2025-04-11 22:32:29 -0300

    Update 6 - moment.tz() working
    - finishing the moment settup
    - making sure it works

--- src/index.js
+++ src/index.js
@@ -1,28 +1,46 @@
-function updateBrasilia() {
-    let brasilia = document.querySelector("#brasilia");
-    let brasiliaDate = brasilia.querySelector(".date");
-    let brasiliaTime = brasilia.querySelector(".time");
-    let brasiliaTZ = moment().tz("America/Sao_Paulo");
+ function updateRioJaneiro() {
+    let rio = document.querySelector("#rio-janeiro");
+    let rioDate = rio.querySelector(".date");
+    let rioTime = rio.querySelector(".time");
+    let rioTZ = moment().tz("America/Sao_Paulo");
 
-    brasiliaDate.innerHTML = brasiliaTZ.format(`MMMM Do, YYYY`);
-    brasiliaTime.innerHTML = brasiliaTZ.format(`hh:mm:ss [<small>]A[</small>]`);
+    rioDate.innerHTML = rioTZ.format(`MMMM Do, YYYY`);
+    rioTime.innerHTML = rioTZ.format(`hh:mm:ss [<small>]A[</small>]`);  
 }
 
+function updateSaoPaulo() {
+    let saoPaulo = document.querySelector("#sao-paulo");
+   let saoPauloDate = saoPaulo.querySelector(".date");
+    let saoPauloTime = saoPaulo.querySelector(".time");
+    let saoPauloTZ = moment().tz("America/Sao_Paulo");
 
-function updateRioBranco() {
-    let rioBranco = document.querySelector("#rio-branco");
-    let rioBrancoDate = rioBranco.querySelector(".date");
-    let rioBrancoTime = rioBranco.querySelector(".time");
-    let rioBrancoTZ = moment().tz("America/Rio_Branco");
-
-    rioBrancoDate.innerHTML = rioBrancoTZ.format(`MMMM Do, YYYY`);
-    rioBrancoTime.innerHTML = rioBrancoTZ.format(`hh:mm:ss [<small>]A[</small>]`);
+    saoPauloDate.innerHTML = saoPauloTZ.format(`MMMM Do, YYYY`);
+    saoPauloTime.innerHTML = saoPauloTZ.format(`hh:mm:ss [<small>]A[</small>]`);
 }
 
 function updateTime() {
-    updateRioBranco();
-    updateBrasilia();
+    updateRioJaneiro();
+    updateSaoPaulo();
+}
+
+function updateCity(event) {
+    const cityToTZ = {
+        "salvador": "America/Sao_Paulo",
+        "rio-branco": "America/Rio_Branco",
+        "belo-horizonte": "America/Sao_Paulo",
+        "cuiaba": "America/Cuiaba",
+        "fernando-noronha": "America/Noronha",
+    }
+
+    let city = event.target.value;
+    let cityTimeZone = cityToTZ[city];
+    let cityTime = moment().tz(cityTimeZone);
+    let cities = document.querySelector("#cities");
+    cities.innerHTML = cityTimeZone;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelect = document.querySelector("#choose-city");
+citySelect.addEventListener("change", updateCity);
